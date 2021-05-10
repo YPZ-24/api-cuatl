cb19426d712efd0591ff9e0d31d9c1be727ffc4a
YPZ-24 <aylinypz@gmail.com>
2021-05-10 01:52:59 -0500

    ssl

--- config/server.js
+++ config/server.js
@@ -7,9 +7,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://cuatl.shop/api',
+  url: 'https://cuatl.shop/api',
   admin: {
-    url: 'http://cuatl.shop/dashboard',
+    url: 'https://cuatl.shop/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
